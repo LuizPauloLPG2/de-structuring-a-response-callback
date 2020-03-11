9aea63d1752a99ecac81bb9685bbdc5bff89e3f6
Luiz Paulo <37484860+luizpaulogroup@users.noreply.github.com>
2020-03-11 19:42:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-axios({
-        method: 'get',
-        url: 'https://api.github.com/users/LuizPauloLPG2/repos',
-    })
-    .then((callback) => {
-        const { data: dados, status, statusText: msg } = callback;
-        console.log(dados);
-        console.log(status);
-        console.log(msg);
+getProfileOnGithub = async () => {
+        
+        try{
+         
+                const response = await api.get('https://api.github.com/users/LuizPauloLPG2/repos');
 
-        for (var i = 0; i < dados.length; i++) {
-            console.log(dados[i].name);
+                const { data: dados, status, statusText: msg } = response;
+                
+                console.log(dados);
+                console.log(status);
+                console.log(msg);
+                
+        }catch(error){
+                console.log(error);     
         }
-});
+        
+}
